bd9465558c1a0f491b5df49462fb79f61e6742a1
lamhung <lamhungypl@gmail.com>
2019-12-30 15:45:24 +0700

    add react-native-vector-icons

--- App/Containers/Home.js
+++ App/Containers/Home.js
@@ -3,7 +3,8 @@ import {View, Text, Button, TouchableOpacity, StyleSheet} from 'react-native';
 import {RouteNames} from '../Config/Routes';
 import CardView from '../Components/CardView';
 import ButtonCustom from '../Components/ButtonCustom';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
+Icon.loadFont();
 const Home = props => {
   const goWeatherDetail = () => {
     props.navigation.navigate(RouteNames.WeatherDetailScreen);
@@ -12,6 +13,7 @@ const Home = props => {
     <View style={styles.container}>
       <TouchableOpacity onPress={() => goWeatherDetail()}>
         <CardView></CardView>
+        <Icon name="rocket" size={30} color="#900" />
       </TouchableOpacity>
     </View>
   );
